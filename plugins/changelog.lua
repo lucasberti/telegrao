5005083b407a8fc55780eecdbc4fe7393ec385b1
lucasberti <berti.lucas@gmail.com>
2015-12-13 17:23:44 -0200

    Plugin de changelog(/changelog)

---
+++ plugins/changelog.lua
@@ -0,0 +1,12 @@
+local function run(msg, matches)
+	return "aooowwww taew o cehngelog::: https://github.com/lucasberti/telegrao/commits/master"
+end
+
+return {
+	description = "",
+	usage = "",
+	patterns = {
+		"^[!|/]changelog"
+	},
+	run = run
+}
