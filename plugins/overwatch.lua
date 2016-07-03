a2c7650c287e5b2d67c264e542b94767d1e0884c
lucasberti <berti.lucas@gmail.com>
2016-07-03 01:11:27 -0300

    Adicionando mais stat competitivo e corrigindo lookup

--- plugins/overwatch.lua
+++ plugins/overwatch.lua
@@ -38,6 +38,9 @@ local function queryAllHeroes(who, mode)
 
 	local multi = resp.Multikills or 0
 	local multi_best = resp.Multikill_Best or 0
+	local defensive_assists = resp.DefensiveAssists:gsub(',', '.') or 0
+	local offensive_assists = resp.OffensiveAssists:gsub(',', '.') or 0
+	local total_assists = tonumber(offensive_assists) + tonumber(defensive_assists)
 
 	local text = ""
 
@@ -46,7 +49,7 @@ local function queryAllHeroes(who, mode)
 	")\nmutikils:: " .. multi .. "(o mas fera foi matano " .. multi_best .. " nobs d 1 ves kkkk)" ..
 	")\nmiojos comido:: " .. resp.FinalBlows .. " .... media:: " .. resp.FinalBlows_Average ..
 	"\nmoreu::: " .. resp.Deaths .. " .... media:: " .. resp.Deaths_Average .. " (" .. resp.EnvironmentalDeaths .. " foran suicdo no mudo kkk)" ..
-	"\nasistss::: " .. resp.DefensiveAssists + resp.OffensiveAssists .. " (" .. resp.DefensiveAssists .. " na dfesa " .. resp.OffensiveAssists .. " n atacke)" ..
+	"\nasistss::: " .. total_assists .. " (" .. resp.DefensiveAssists .. " na dfesa " .. resp.OffensiveAssists .. " n atacke)" ..
 	"\ndano q ja deu:: " .. resp.DamageDone ..	" (record:: " .. resp.DamageDone_MostinGame .. " .... media:: " .. resp.DamageDone_Average .. 
 	")\nkura q ja fes:: " .. resp.HealingDone ..	" (record:: " .. resp.HealingDone_MostinGame .. " .... media:: " .. resp.HealingDone_Average ..
 	")\ntenpo n obetivo:: " .. resp.ObjectiveTime ..	" (record:: " .. resp.ObjectiveTime_MostinGame .. " .... media:: " .. resp.ObjectiveTime_Average ..
@@ -87,58 +90,58 @@ local function queryHeroes(who, mode)
 end
 
 local function queryProfile(who, mode)
-	local url_ow = url .. who
-
-	if mode ~= nil and string.find(mode, "comp") then
-		url_ow = url_ow .. "/competitive-play/profile"
-	else
-		url_ow = url_ow .. "/quick-play/profile"
-	end
+	local url_ow = url .. who .. "/profile"
 
 	print(url_ow)
 
 	local res, code = https.request(url_ow)
 	local resp = json:decode(res)
 
-	local text = resp.data.username
-
-	if not string.find(mode, "comp") then
-		text = text .. " jogo " .. toEdLanguage(resp.data.playtime) .. " i "
-	end
-
-	text = text .. " ten lelve " .. resp.data.level ..
-	".\njogo " .. resp.data.games.played ..
-	" pratidias perdeu " .. resp.data.games.lost ..
-	" i ganho " .. resp.data.games.wins ..
-	" entao tem %¨ di ganhagen de " .. resp.data.games.win_percentage .. "¨&¨%\n\n"
+	local text = resp.data.username .. " jogo " .. toEdLanguage(resp.data.playtime.quick) ..
+	"\nten lelve " .. resp.data.level ..
+	" i jogo " .. resp.data.games.quick.played ..
+	" pratidias, ganho " .. resp.data.games.quick.wins ..
+	" i perdeu " .. resp.data.games.quick.lost .. "\n\n" ..
+	"no conp ja gasto " .. toEdLanguage(resp.data.playtime.competitive) .. " i ta co rank " .. resp.data.competitive.rank .. ".\n" ..
+	"jogo " .. resp.data.games.competitive.played ..
+	" pratidias, ganho " .. resp.data.games.competitive.wins ..
+	" i perdeu " .. resp.data.games.competitive.lost .. "\n\n"
 
 	return text .. queryAllHeroes(who, mode) .. queryHeroes(who, mode)
 end
 
 function run(msg, matches)
 	user_id = tostring(msg.from.id)
-	local mode = string.lower(matches[1]) or nil
-	
-	-- Eu
-	if user_id == "14160874" then
-		return queryProfile("berti-11937", mode)
+
+	local mode = nil
+
+	if #matches == 2 then
+		mode = string.lower(matches[2])
 	end
 
-	-- Bea
-	if user_id == "16631085" then
-		return queryProfile("bea-11259", mode)
-	end
+	if #matches > 1 and not string.find(mode, "comp") then
+		return queryProfile(mode, nil)
+	else
+		-- Eu
+		if user_id == "14160874" then
+			return queryProfile("berti-11937", mode)
+		end
 
-	-- Retcha
-	if user_id == "85867003" then
-		return queryProfile("Retcha-11793", mode)
-	end
+		-- Bea
+		if user_id == "16631085" then
+			return queryProfile("bea-11259", mode)
+		end
 
-	-- Tiko
-	if user_id == "80048195" then
-		return queryProfile("Tikomico-1650", mode)
-	end
+		-- Retcha
+		if user_id == "85867003" then
+			return queryProfile("Retcha-11793", mode)
+		end
 
+		-- Tiko
+		if user_id == "80048195" then
+			return queryProfile("Tikomico-1650", mode)
+		end
+	end
 end
 
 
@@ -146,10 +149,10 @@ return {
 	description = "Stats do overwatch", 
 	usage = "!ow, /overwatch",
 	patterns = {
-		"^[!/]overwatch$",
-		"^[!/]ow$",
-		"^[!/]overwatch (.*)$",
-		"^[!/]ow (.*)$",
+		"^[!/](overwatch)$",
+		"^[!/](ow)$",
+		"^[!/](overwatch) (.*)$",
+		"^[!/](ow) (.*)$",
 		"^[!/]overwatch@PintaoBot$"
 	}, 
 	run = run 
